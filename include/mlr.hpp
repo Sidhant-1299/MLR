5c1a2e61d4b202281cff1ce17a18c9a231d49892
Sidhant-1299 <sidhant.raj.khati@gmail.com>
2025-05-17 11:29:13 -0400

    Added tests

--- include/mlr.hpp
+++ include/mlr.hpp
@@ -8,6 +8,6 @@ private:
 
 public:
     void fit(const Eigen::MatrixXd &, const Eigen::MatrixXd &);
-    Eigen::VectorXd predict(const Eigen::MatrixXd &) const;
+    Eigen::MatrixXd predict(const Eigen::MatrixXd &) const;
     Eigen::MatrixXd coefficients() const;
 };
