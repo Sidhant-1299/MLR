24f24482f6a9909103c8c130b539b2b6112a47dc
Sidhant-1299 <sidhant.raj.khati@gmail.com>
2025-05-23 16:00:27 -0400

    changed build instructions for win32 and win64

--- setup.py
+++ setup.py
@@ -6,14 +6,16 @@ import sys
 
 # Try to detect if we're building in like GitHub Actions
 IS_CI = os.environ.get("CI", "false").lower() == "true"
+is_windows = sys.platform == ("win32" or "win64")
+
 
 # Use external/ Eigen and Boost by default for sdist
 default_eigen_path = os.path.abspath("external/eigen-3.4.0")
 default_boost_path = os.path.abspath("external/boost_1_88_0")
 
 #point to system wide eigen and boost for CI
-eigen_path = "/usr/include/eigen3" if IS_CI else default_eigen_path
-boost_path = "/usr/include" if IS_CI else default_boost_path
+eigen_path = "/usr/include/eigen3" if (IS_CI and not is_windows) else default_eigen_path
+boost_path = "/usr/include" if (IS_CI and not is_windows) else default_boost_path
 
 class BuildExtWithCheck(build_ext):
     def build_extensions(self):
