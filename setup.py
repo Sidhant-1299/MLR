cf1a243bf9de867be837208e7f58c8d281cbcc77
Sidhant-1299 <sidhant.raj.khati@gmail.com>
2025-05-23 15:48:23 -0400

    added setup for both bdist and sdist; changed build.yml for CI installation

--- setup.py
+++ setup.py
@@ -2,27 +2,40 @@ from setuptools import setup, Extension, find_packages
 from setuptools.command.build_ext import build_ext
 import pybind11
 import os
+import sys
 
-eigen_path = os.path.abspath("external/eigen-3.4.0")
-boost_path = os.path.abspath("externa;/boost1_88_0")
+# Try to detect if we're building in like GitHub Actions
+IS_CI = os.environ.get("CI", "false").lower() == "true"
+
+# Use bundled Eigen and Boost by default
+default_eigen_path = os.path.abspath("external/eigen-3.4.0")
+default_boost_path = os.path.abspath("external/boost_1_88_0")
+
+# If system-wide Boost/Eigen are preferred in CI, point there
+eigen_path = "/usr/include/eigen3" if IS_CI else default_eigen_path
+boost_path = "/usr/include" if IS_CI else default_boost_path
+
+class BuildExtWithCheck(build_ext):
+    def build_extensions(self):
+        # Add include paths
+        for ext in self.extensions:
+            ext.include_dirs.append(eigen_path)
+            ext.include_dirs.append(boost_path)
+            ext.include_dirs.append(pybind11.get_include())
+            ext.include_dirs.append(pybind11.get_include(user=True))
+        super().build_extensions()
 
 ext_modules = [
     Extension(
         'mlr_cpp',
         sources=['src/mlr_cpp.cpp', 'src/bindings.cpp'],
         include_dirs=[
-            "include",
-            eigen_path,
-            boost_path,
-            pybind11.get_include(),
-            pybind11.get_include(user=True),
+            "include",  
         ],
         language='c++',
         extra_compile_args=['-std=c++20']
     )
 ]
-
-
 
 setup(
     name='mlr_cpp',
@@ -33,6 +46,6 @@ setup(
     include_package_data=True,
     description="A high-performance Multiple Linear Regression model implemented in C++ with a clean Python API via Pybind11 bindings",
     author="Sidhant Raj Khati",
-    cmdclass={'build_ext': build_ext},
+    cmdclass={'build_ext': BuildExtWithCheck},
     zip_safe=False,
 )
